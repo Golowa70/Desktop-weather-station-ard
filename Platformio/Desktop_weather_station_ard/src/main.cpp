aef89cfba8f737f19f206f9a83f05d48d58f1d2d
Golowa70 <golowa70@gmail.com>
2021-06-06 23:48:36 +0300

    changes

--- Platformio/Desktop_weather_station_ard/src/main.cpp
+++ Platformio/Desktop_weather_station_ard/src/main.cpp
@@ -8,13 +8,14 @@
 //#include <Fonts/FreeSansBold18pt7b.h>
 #include <Fonts/FreeMonoBold12pt7b.h>
 //#include <Fonts/FreeSerifBold24pt7b.h>
-#include <EncButton.h>
 //#include "GyverButton.h"
 //#include <microDS3231.h>
 #include <Wire.h>
 #include <ErriezDS1307.h>
 #include <microDS18B20.h>
-
+#include "GyverEncoder.h"
+//#include <GyverPower.h>
+#include "LowPower.h"
 
 
 
@@ -22,33 +23,45 @@
 #define TFT_DC  8
 #define TFT_RST 9
 #define TFT_LED 6
-//#define BUTTON  7
-#define ENC_A       4
-#define ENC_B       5
+
+#define ENC_A       2
+#define ENC_B       3
 #define ENC_BUTTON  7 
 #define SENSOR_PIN  A0
+#define DS18B20_POWER_OUT 4
+#define DS1307_POWER_OUT  5
 
-EncButton<EB_TICK, ENC_A, ENC_B, ENC_BUTTON> enc(INPUT_PULLUP);        // энкодер с кнопкой <A, B, KEY>
 Arduino_ILI9163C lcd = Arduino_ILI9163C(TFT_DC, TFT_RST, TFT_CS);
 ErriezDS1307 rtc;
 MicroDS18B20 sensor(SENSOR_PIN);
 
-uint8_t cnt;
+Encoder enc1(ENC_A, ENC_B, ENC_BUTTON);
+
+volatile uint8_t cnt;
 bool flag_change;
+bool flag_sleep;
 uint8_t hour;
 uint8_t min;
 uint8_t sec;
 uint8_t old_sec;
 uint8_t temp;
+uint8_t old_temp;
 
 void Wakeup();
 
 
 
+
 void setup() {
 
-  //pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(ENC_A, INPUT_PULLUP);
+  pinMode(ENC_B, INPUT_PULLUP);
+  pinMode(ENC_BUTTON, INPUT_PULLUP);
   pinMode(TFT_LED, OUTPUT);
+  pinMode(DS18B20_POWER_OUT,OUTPUT);
+  pinMode(DS1307_POWER_OUT,OUTPUT);
+  digitalWrite(DS18B20_POWER_OUT,HIGH);
+  digitalWrite(DS1307_POWER_OUT,HIGH);
   analogWrite(TFT_LED, 127);
   Serial.begin(9600);
   lcd.init();
@@ -57,49 +70,54 @@ void setup() {
   lcd.setTextColor(WHITE,BLUE);
   lcd.setFont(&FreeMonoBold12pt7b );
   //lcd.setFont(&FreeSansBold18pt7b);
-
   //lcd.setFont(&FreeSerifBold24pt7b);
   
-  enc.counter = 100;      // изменение счётчика
- 
   attachInterrupt(digitalPinToInterrupt(ENC_A), Wakeup,CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENC_B), Wakeup,CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENC_BUTTON), Wakeup,CHANGE);
-
+ 
   Wire.begin();
   Wire.setClock(100000);
   rtc.begin();
   rtc.setSquareWave(SquareWaveDisable);
-  rtc.setTime(12, 0, 0);
+  //rtc.setTime(12, 0, 0);
   rtc.getTime(&hour, &min, &sec);
   //lcd.sleepDisplay(1);
   //analogWrite(TFT_LED, 0);
 
   sensor.setResolution(9);
 
+  //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
+ // LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+ 
   
 }
 
 void loop() {
 
-  enc.tick();
-  lcd.setCursor(10, 60);
-  lcd.setTextSize(3);
-  lcd.println("27");
-  lcd.setTextSize(1);
-  lcd.setCursor(95, 30);
-  lcd.println("o");
+  enc1.tick();
 
-  //lcd.setTextSize(1);
-  //lcd.setCursor(5, 100);
-  //lcd.println(sec);
+  if(temp != old_temp){
+    lcd.fillRect(10,10,90,60,BLUE);
+    lcd.setCursor(10, 60);
+    lcd.setTextSize(3);
+    lcd.println(temp);
+    lcd.setTextSize(1);
+    lcd.setCursor(95, 30);
+    lcd.println("o");
+    old_temp = temp;
+    
+  }
+
+  
   rtc.getTime(&hour, &min, &sec);
   if(sec != old_sec){
 
     rtc.getTime(&hour, &min, &sec);
-    lcd.fillRect(20,70,40,40,BLUE);
+    lcd.fillRect(20,86,28,15,BLUE);
     lcd.setCursor(20, 100);
-    lcd.println(sec);
+    //lcd.print("   ");
+    lcd.print(sec);
     old_sec = sec;
 
     if(flag_change)sensor.requestTemp(); 
@@ -115,24 +133,57 @@ void loop() {
     Serial.println(temp);
   }
 
-  if(enc.isTurn()){
-    
-    cnt++;
-    lcd.sleepDisplay(0);
-    analogWrite(TFT_LED, 127);
-    
+  if(enc1.isRight()){
+    cnt++;   
+  }
+
+  if(enc1.isLeft()){
+    cnt--;   
+  }
+
+  if(enc1.isPress()){
+    analogWrite(TFT_LED, 0);
+    lcd.sleepDisplay(1);
+    digitalWrite(DS18B20_POWER_OUT,LOW);
+    digitalWrite(DS1307_POWER_OUT,LOW);
+    flag_sleep = true;
+
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 
   }
       
+  lcd.fillRect(60,110,30,20,BLUE);
+  lcd.setCursor(60, 128);
+  lcd.print(cnt);
 
-  delay(100);
+
+  delay(1);
   
   
 }
 
 //**********************************************
 void Wakeup(void) {
+  
+  enc1.tick();
+ 
+  if(flag_sleep){
+    cli();
+    digitalWrite(DS18B20_POWER_OUT,HIGH);
+    digitalWrite(DS1307_POWER_OUT,HIGH);
+    delay(500);
+   // Wire.begin();
+   // Wire.setClock(100000);
+   // rtc.begin();
+    //rtc.getTime(&hour, &min, &sec);
+   // rtc.setSquareWave(SquareWaveDisable);
+    lcd.sleepDisplay(0);
+    analogWrite(TFT_LED, 127);
+    flag_sleep = false;
+    sei();
 
-  enc.tick();
+  }
+  
 
 }
+
